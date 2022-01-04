a54f5effb22d59752bd15aaead1078000285387f
Whoistolu <digittolu25@gmail.com>
2022-01-04 14:59:31 +0100

    Fix more linter errors

--- author.rb
+++ author.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Author
   attr_accessor :first_name, :last_name, :id
   attr_reader :items
--- game.rb
+++ game.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Game
   attr_accessor :multiplayer, :last_played_at
 
