e791e0df32b36b269dad179b2304a5fb87c4f628
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-06 13:24:28 +0200

    Add create_genre

---
+++ lib/modules/create_genre.rb
@@ -0,0 +1,16 @@
+require_relative 'mainMethods'
+
+module GenreModule
+  include MainMethods
+
+  def genre_list
+    if @genres.length.zero?
+      puts 'No genres entered yet.'
+    else
+      puts "List of all genres:\n"
+      @genres.each_with_index do |genre, index|
+        print "#{index}. #{genre.name}"
+      end
+    end
+  end
+end
