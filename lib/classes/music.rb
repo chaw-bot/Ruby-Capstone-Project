634c3a1a1aaf988f93edf042d67cf4933f07183b
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-06 13:22:00 +0200

    Refactor class

--- lib/classes/music.rb
+++ lib/classes/music.rb
@@ -1,11 +1,14 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify
+  attr_accessor :on_spotify, :publish_date
+  attr_reader :items
 
-  def initialize(on_spotify, *args)
-    super(*args)
+  def initialize(on_spotify, publish_date)
+    super(publish_date)
+    @id = Random.rand(1..1000)
     @on_spotify = on_spotify
+    @items = []
   end
 
   def can_be_archived?
